3feb01f2fa1d7eab2922d3cd47923c4914b3dfdf
amanhanda1 <amanhanda446@gmail.com>
2022-07-09 21:08:28 +0530

    hi

--- reverseinarray.cpp
+++ reverseinarray.cpp
@@ -5,10 +5,10 @@ int main(){
     int arr[5]={1,2,3,4,7};
     int s=0;
     int e=n-1;
-    // for(s=0;s<e;s++,e--){
-    //    swap(arr[s],arr[e]);
-    // }
-    for(int i=0;i<n&&i>0;i--){
+    for(s=0;s<e;s++,e--){
+       swap(arr[s],arr[e]);
+    }
+    for(int i=0;i<n;i++){
         std::cout<<arr[i];
         }
     return 0;
